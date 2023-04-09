877a52986daafd902853d8e63314695ec1409732
zorro901 <zoro907@gmail.com>
2023-04-09 13:47:59 +0900

    :package: build: expoの実行環境を追加

--- apps/expo/babel.config.js
+++ apps/expo/babel.config.js
@@ -1,5 +1,6 @@
+/** @type {import("@babel/core").ConfigFunction} */
 module.exports = function(api) {
-  api.cache(true);
+  api.cache.forever();
   return {
     presets: ['babel-preset-expo'],
   };
--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -1,14 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const Index = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
+
+export default Index
 
 const styles = StyleSheet.create({
   container: {
