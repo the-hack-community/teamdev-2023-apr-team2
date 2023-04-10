aa7935a35cd3fe4a366e2c62c456d4569a69d6c4
zorro901 <zoro907@gmail.com>
2023-04-10 09:42:47 +0900

    :package: build: jestを追加

--- apps/expo/babel.config.js
+++ apps/expo/babel.config.js
@@ -3,11 +3,7 @@ module.exports = function (api) {
   api.cache.forever()
   process.env.EXPO_ROUTER_APP_ROOT = '../../apps/expo/src/app'
   return {
-    plugins: [
-      'nativewind/babel',
-      require.resolve('expo-router/babel'),
-      'react-native-reanimated/plugin',
-    ],
+    plugins: ['nativewind/babel', require.resolve('expo-router/babel')],
     presets: ['babel-preset-expo'],
   }
 }
--- apps/expo/index.tsx
+++ apps/expo/index.tsx
@@ -2,9 +2,9 @@ import { registerRootComponent } from 'expo'
 import { ExpoRoot } from 'expo-router'
 
 // Must be exported or Fast Refresh won't update the context
-const App = () => {
+const Root = () => {
   const ctx = require.context('./src/app')
   return <ExpoRoot context={ctx} />
 }
 
-registerRootComponent(App)
+registerRootComponent(Root)
