9b1ecb98c53a83a84345bf4bd15a9ed6949e8e40
zorro901 <zoro907@gmail.com>
2023-05-02 02:20:04 +0900

    :bug: fix(front): 環境変数を読み込む設定を追加

--- apps/expo/babel.config.js
+++ apps/expo/babel.config.js
@@ -3,7 +3,11 @@ module.exports = function (api) {
   api.cache.forever()
   process.env.EXPO_ROUTER_APP_ROOT = '../../apps/expo/src/app'
   return {
-    plugins: ['nativewind/babel', require.resolve('expo-router/babel')],
+    plugins: [
+      'transform-inline-environment-variables',
+      'nativewind/babel',
+      require.resolve('expo-router/babel'),
+    ],
     presets: ['babel-preset-expo'],
   }
 }
