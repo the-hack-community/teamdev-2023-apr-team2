b05377137989635ed232d0764e2d219910415ed5
zorro901 <zoro907@gmail.com>
2023-05-11 23:42:00 +0900

    :package: build(front): プロジェクト関連の設定を更新
    認証関係のパッケージを追加

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -1,38 +1,43 @@
-import type { ExpoConfig } from "@expo/config";
+import type { ExpoConfig } from '@expo/config'
 
-const defineConfig = (): ExpoConfig => ({
-  name: "expo",
-  slug: "expo",
-  scheme: "expo",
-  version: "1.0.0",
-  orientation: "portrait",
-  icon: "./assets/icon.png",
-  userInterfaceStyle: "light",
+type Config = {
+  scheme: string[] | string
+}
+
+const defineConfig = (): Omit<ExpoConfig, 'scheme'> & Config => ({
+  name: 'expo',
+  slug: 'cilotta',
+  scheme: ['cilotta', 'fb629191368747614'],
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  userInterfaceStyle: 'light',
   splash: {
-    image: "./assets/icon.png",
-    resizeMode: "contain",
-    backgroundColor: "#1F104A",
+    image: './assets/icon.png',
+    resizeMode: 'contain',
+    backgroundColor: '#1F104A',
   },
   updates: {
     fallbackToCacheTimeout: 0,
   },
-  assetBundlePatterns: ["**/*"],
+  assetBundlePatterns: ['**/*'],
   ios: {
     supportsTablet: true,
-    bundleIdentifier: "your.bundle.identifier",
+    bundleIdentifier: 'your.bundle.identifier',
   },
   android: {
     adaptiveIcon: {
-      foregroundImage: "./assets/icon.png",
-      backgroundColor: "#1F104A",
+      foregroundImage: './assets/icon.png',
+      backgroundColor: '#1F104A',
     },
+    package: 'app.expo.cilotta',
   },
   extra: {
     eas: {
-      projectId: "your-project-id",
+      projectId: '0097aebd-d4a8-4e7d-89a0-9b24ceb1112f',
     },
   },
-  plugins: ["./expo-plugins/with-modify-gradle.js"],
-});
+  plugins: ['./expo-plugins/with-modify-gradle.js'],
+})
 
-export default defineConfig;
+export default defineConfig
