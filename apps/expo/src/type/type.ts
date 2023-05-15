8fdb5cb463688bdca90f6771fa447e2370beabf1
zorro901 <zoro907@gmail.com>
2023-05-15 16:30:22 +0900

    :sparkles: feat(front): 駐輪場のデータ型を追加

---
+++ apps/expo/src/type/type.ts
@@ -0,0 +1,19 @@
+export type ParkingBaseInfo = {
+  id: number
+  name: string
+  address: string
+  usage_time: string
+  capacity: number
+  multi_storey: boolean
+  with_roof: boolean
+  free_hour: number
+  onetime_price: number
+}
+
+export type ParkingLocation = {
+  id: number
+  latitude: number
+  longitude: number
+}
+
+export type ParkingInfo = ParkingLocation & ParkingBaseInfo
