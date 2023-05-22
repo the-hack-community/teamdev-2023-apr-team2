cb8cb06d3f8df86ede4c6c2b20f7a6bd30c940d3
zorro901 <zoro907@gmail.com>
2023-05-23 08:41:21 +0900

    :sparkles: feat(front): プッシュ通知を実装

--- apps/expo/index.tsx
+++ apps/expo/index.tsx
@@ -2,6 +2,7 @@ import 'expo-router/entry'
 import 'react-native-reanimated'
 import 'react-native-gesture-handler'
 
+import messaging from '@react-native-firebase/messaging'
 import { registerRootComponent } from 'expo'
 import { ExpoRoot } from 'expo-router'
 
@@ -10,5 +11,11 @@ const Root = () => {
   const ctx = require.context('./src/app')
   return <ExpoRoot context={ctx} />
 }
+messaging().setBackgroundMessageHandler(
+  (remoteMessage) =>
+    new Promise((resolve) => {
+      resolve(remoteMessage)
+    }),
+)
 
 registerRootComponent(Root)
