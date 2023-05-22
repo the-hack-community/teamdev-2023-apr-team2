974da60497f69d5182246edc69f7eb68c0944c66
zorro901 <zoro907@gmail.com>
2023-05-23 08:34:06 +0900

    :sparkles: feat(back): プッシュ通知を実装

--- apps/server/controller/parking_controller.go
+++ apps/server/controller/parking_controller.go
@@ -1,8 +1,16 @@
 package controller
 
 import (
+	"context"
+	"encoding/base64"
+	firebase "firebase.google.com/go"
+	"firebase.google.com/go/messaging"
+	"fmt"
 	"github.com/labstack/echo/v4"
+	"google.golang.org/api/option"
+	"log"
 	"net/http"
+	"os"
 	"server/model"
 	"server/usecase"
 	"strconv"
@@ -49,5 +57,44 @@ func (pc *parkingController) CreateParking(c echo.Context) error {
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, err.Error())
 	}
+
+	// Send push notification
+	ctx := context.Background()
+	conf := &firebase.Config{
+		ProjectID: "cilotta-386405",
+	}
+	base64Credential := os.Getenv("FIREBASE_KEYFILE_JSON")
+	jsonCredential, err := base64.StdEncoding.DecodeString(base64Credential)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+	opt := option.WithCredentialsJSON(jsonCredential)
+	app, err := firebase.NewApp(ctx, conf, opt)
+	if err != nil {
+		log.Fatalf("Failed to initialize Firebase app: %v\n", err)
+	}
+	sendToToken(app)
+
 	return c.JSON(http.StatusOK, parkingRes)
+}
+
+func sendToToken(app *firebase.App) {
+	ctx := context.Background()
+	client, err := app.Messaging(ctx)
+	if err != nil {
+		log.Fatalf("error getting Messaging client: %v\n", err)
+	}
+	message := &messaging.Message{
+		Notification: &messaging.Notification{
+			Title: "新しい駐輪場が登録されました",
+			Body:  "新しい駐輪場が登録されました",
+		},
+		Topic: "cilotta",
+	}
+	response, err := client.Send(ctx, message)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("Successfully sent message:", response)
 }
